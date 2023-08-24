363f2d3ce0327f03a5357d65e0e51aa70e1ac23d
deekshithepns <70252174+deekshithmd@users.noreply.github.com>
2023-08-24 23:18:40 +0530

    added promises

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Progress from './components/Progress';
 import Rating from './components/Rating';
+import { PromiseAll } from './components/PromiseAll';
 
 function App() {
   return (
     <div className="App">
       <Progress />
       <Rating />
+      <PromiseAll />
     </div>
   );
 }
---
+++ src/components/PromiseAll/index.jsx
@@ -0,0 +1,33 @@
+
+const myPromiseAll = (promiseList) => {
+    const result = [];
+    let completedPromises = 0;
+    return new Promise((resolve, reject) => {
+        promiseList.forEach(promise => {
+            promise.then((val) => {
+                result.push(val);
+                completedPromises += 1;
+
+                if (promiseList.length === completedPromises) {
+                    resolve(result)
+                }
+            })
+                .catch(e => reject(e))
+        })
+    })
+}
+
+export const PromiseAll = () => {
+
+    const task = (time) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => time > 500 ? reject("Rejected") : resolve(time), time);
+        })
+    }
+
+    const promises = [task(500), task(300), task(700)]
+
+    myPromiseAll(promises).then(result => console.log("got", result)).catch(e => console.log("failed", e))
+
+    return <h1>Promise All</h1>
+}
