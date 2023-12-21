d9fcc173e61590d6fc2a4962eb1cad2cbb4890bc
deekshithmd <deekshithmogra@gmail.com>
2023-12-21 22:56:02 +0530

    updated variable

--- src/components/DataStructures/Tree/index.js
+++ src/components/DataStructures/Tree/index.js
@@ -1,132 +1,123 @@
 import React from "react";
 
 class Node {
-    constructor(data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(value) {
+    this.data = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
 class Tree {
-    constructor() {
-        this.root = null;
-    }
-    findMinNode(node) {
-        if (node.left == null) {
-            return node
-        }
-        else this.findMinNode(node.left)
-    }
+  constructor() {
+    this.root = null;
+  }
+  findMinNode(node) {
+    if (node.left == null) {
+      return node;
+    } else this.findMinNode(node.left);
+  }
 
-    insert(data) {
-        const node = new Node(data);
-        if (this.root == null) {
-            this.root = node;
-        }
-        else {
-            this.insertNode(this.root, node)
-        }
+  insert(data) {
+    const node = new Node(data);
+    if (this.root == null) {
+      this.root = node;
+    } else {
+      this.insertNode(this.root, node);
     }
-    insertNode(node, newNode) {
-        if (newNode.data < node.data) {
-            if (node.left == null) {
-                node.left = newNode
-            }
-            else {
-                this.insertNode(node.left, newNode)
-            }
-        }
-        else {
-            if (node.right == null) {
-                node.right = newNode;
-            }
-            else {
-                this.insertNode(node.right, newNode)
-            }
-        }
+  }
+  insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+      if (node.left == null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right == null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
     }
-    remove(data) {
-        this.root = this.removeNode(this.root, data)
-    }
-    removeNode(node, key) {
-        if (node == null)
-            return null;
-        else if (key < node.data) {
-            node.left = this.removeNode(node.left, key)
-            return node;
-        }
-        else if (key > node.data) {
-            node.right = this.removeNode(node.right, key)
-            return node;
-        }
-        else {
-            if (node.left == null && node.right == null) {
-                node = null;
-                return node;
-            }
-            if (node.left == null) {
-                node = node.right;
-                return node;
-            }
-            else if (node.right == null) {
-                node = node.left;
-                return node;
-            }
-            const aux = this.findMinNode(node.right);
-            node.data = aux.data;
+  }
+  remove(data) {
+    this.root = this.removeNode(this.root, data);
+  }
+  removeNode(node, key) {
+    if (node == null) return null;
+    else if (key < node.data) {
+      node.left = this.removeNode(node.left, key);
+      return node;
+    } else if (key > node.data) {
+      node.right = this.removeNode(node.right, key);
+      return node;
+    } else {
+      if (node.left == null && node.right == null) {
+        node = null;
+        return node;
+      }
+      if (node.left == null) {
+        node = node.right;
+        return node;
+      } else if (node.right == null) {
+        node = node.left;
+        return node;
+      }
+      const aux = this.findMinNode(node.right);
+      node.data = aux.data;
 
-            node.right = this.removeNode(node.data, aux.data);
-            return node;
-        }
+      node.right = this.removeNode(node.data, aux.data);
+      return node;
     }
-    inorder(node) {
-        if (node !== null) {
-            this.inorder(node.left)
-            console.log(node.data);
-            this.inorder(node.right);
-        }
+  }
+  inorder(node) {
+    if (node !== null) {
+      this.inorder(node.left);
+      console.log(node.data);
+      this.inorder(node.right);
     }
-    preorder(node) {
-        if (node !== null) {
-            console.log(node.data);
-            this.preorder(node.left);
-            this.preorder(node.right)
-        }
+  }
+  preorder(node) {
+    if (node !== null) {
+      console.log(node.data);
+      this.preorder(node.left);
+      this.preorder(node.right);
     }
-    postorder(node) {
-        if (node !== null) {
-            this.postorder(node.left);
-            this.postorder(node.right);
-            console.log(node.data)
-        }
+  }
+  postorder(node) {
+    if (node !== null) {
+      this.postorder(node.left);
+      this.postorder(node.right);
+      console.log(node.data);
     }
-    getRootNode() {
-        return this.root
-    }
+  }
+  getRootNode() {
+    return this.root;
+  }
 }
 
 export const TreeComponent = () => {
-    const tree = new Tree();
-    tree.insert(4);
-    tree.insert(6);
-    tree.insert(3);
-    tree.insert(2);
-    tree.insert(1);
-    tree.insert(8);
-    tree.insert(5);
-    //      4
-    //    3    6
-    //  2    5   8
-    //1
-    tree.remove(2)
-    const root = tree.getRootNode()
-    console.log('inorder')
-    tree.inorder(root);
-    console.log('preorder')
-    tree.preorder(root);
-    console.log('postorder')
-    tree.postorder(root);
+  const tree = new Tree();
+  tree.insert(4);
+  tree.insert(6);
+  tree.insert(3);
+  tree.insert(2);
+  tree.insert(1);
+  tree.insert(8);
+  tree.insert(5);
+  //      4
+  //    3    6
+  //  2    5   8
+  //1
+  tree.remove(2);
+  const root = tree.getRootNode();
+  console.log("inorder");
+  tree.inorder(root);
+  console.log("preorder");
+  tree.preorder(root);
+  console.log("postorder");
+  tree.postorder(root);
 
-    return <h1>Tree</h1>
-}
+  return <h1>Tree</h1>;
+};
