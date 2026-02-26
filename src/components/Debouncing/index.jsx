f66858716dd11e7cec8427bdae1924a7dba5a726
Deekshith <deekshith@Signeasys-MacBook-Pro-2.local>
2026-02-26 11:53:48 +0530

    update: updated debouncing function

--- src/components/Debouncing/index.jsx
+++ src/components/Debouncing/index.jsx
@@ -3,14 +3,58 @@ import React, { useState } from "react"
 export const Debouncing = () => {
     const [data, setData] = useState('')
 
-    const debouncing = (fn, time) => {
-        let id = 0;
-        return function () {
-            const context = this;
-            const args = arguments;
-            clearTimeout(id);
-            id = setTimeout(() => fn.apply(context, args), time)
+    // const debouncing = (fn, time) => {
+    //     let id = 0;
+    //     return function () {
+    //         const context = this;
+    //         const args = arguments;
+    //         clearTimeout(id);
+    //         id = setTimeout(() => fn.apply(context, args), time)
+    //     }
+    // }
+
+    const debouncing = (fn, delay, options = {}) => {
+        let timerId = null;
+        const { leading = false, trailing = true } = options;
+        let lastArgs, lastThis;
+
+
+        function debounce(...args) {
+            lastThis = this;
+            lastArgs = args;
+
+            if (timerId) {
+                clearTimeout(timerId)
+            }
+
+            if (leading && !timerId) {
+                fn.apply(lastThis, lastArgs)
+            }
+
+            timerId = setTimeout(() => {
+                if (trailing) {
+                    fn.apply(lastThis, lastArgs)
+                }
+                timerId = null;
+            }, delay);
         }
+
+        debounce.cancel = function () {
+            if (timerId) {
+                clearTimeout(timerId);
+                timerId = null
+            }
+        }
+
+        debounce.flush = function () {
+            if (timerId) {
+                clearTimeout(timerId)
+                fn.apply(lastThis, lastArgs)
+                timerId = null;
+            }
+        }
+
+        return debounce
     }
 
 
